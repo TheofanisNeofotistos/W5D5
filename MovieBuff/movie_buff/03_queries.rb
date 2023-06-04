702e4e3b70669abe26bb5e660cf1a201787179a8
Manel-Os <oulds.manel@gmail.com>
2023-06-04 19:38:06 -0400

    Completed 03_queries

--- MovieBuff/movie_buff/03_queries.rb
+++ MovieBuff/movie_buff/03_queries.rb
@@ -1,24 +1,48 @@
 def what_was_that_one_with(those_actors)
   # Find the movies starring all `those_actors` (an array of actor names).
   # Show each movie's title and id.
-  
+
+  Movie.joins(:actors)
+    .select(:title, :id)
+    .where(actors: { name: those_actors })
+    .group(:id)
+    .having('COUNT(actors.id) >= ?', those_actors.length)
 end
 
 def golden_age
   # Find the decade with the highest average movie score.
   # HINT: Use a movie's year to derive its decade. Remember that you can use
   # arithmetic expressions in SELECT clauses.
+  Movie.select('(yr / 10) * 10 AS decade')
+    .group('decade')
+    .order('avg(score) DESC')
+    .first
+    .decade
+
   
 end
 
 def costars(name)
   # List the names of the actors that the named actor has ever appeared with.
   # Hint: use a subquery
+  Actor.joins(:movies)
+  .where('movies.id IN (?)',
+    Movie.joins(:actors)
+      .where('actors.name = ?', name)
+      .select(:id)
+  )
+  .where.not('actors.name = ?', name)
+  .distinct
+  .pluck(:name)
   
 end
 
 def actor_out_of_work
   # Find the number of actors in the database who have not appeared in a movie.
+
+  Actor.select(:name)
+     .where.not(id: Casting.select(:actor_id).distinct).count
+ 
   
 end
 
@@ -36,5 +60,9 @@ def longest_career
   # Find the 3 actors who had the longest careers (i.e., the greatest time
   # between first and last movie). Order by actor names. Show each actor's id,
   # name, and the length of their career.
+  Actor.joins(:movies).select(:id,:name,'MAX(movies.yr) - MIN(movies.yr) AS career_length')
+  .group(:id)
+  .order('career_length DESC')
+  .limit(3)
   
 end
